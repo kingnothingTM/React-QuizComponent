2f4d3e09e5a163036fa408a57af9f670f65edd74
kingnothingTM <vjovicic@telmed.rs>
2022-04-26 15:17:10 +0200

    fix #2

--- src/Quiz.js
+++ src/Quiz.js
@@ -17,10 +17,10 @@ class Quiz extends Component{
     }
     render()
     {
-        const isQuiuzEnd = quizData.quiz_questions.length === this.state.quiz_position - 1;
+        const isQuizEnd = quizData.quiz_questions.length === this.state.quiz_position - 1;
         return (<div>
             <div className='QuizQuestion'>
-                 {isQuiuzEnd && <QuizEnd />}
+                 {isQuizEnd && <QuizEnd />}
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
             </div>
         </div>);
